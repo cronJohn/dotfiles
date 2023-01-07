08660787f249779caff91747064298d22f3f5855
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2023-01-07 01:05:10 -0600

    Refactored installer.sh script
    Now it's much simpler and actually works

--- installer.sh
+++ installer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 items=("Item 1" "Item 2" "Item 3")
 PS3=$'\nSelect an item > '
-choices=()
 
 toggleItem(){
     local newText
@@ -9,16 +8,21 @@ toggleItem(){
     local currentItem="${items[$index]}"
 
     if grep -q "✔️" <<< "$currentItem"; then # If currentItem has a ✔️
-        currentItem="$currentItem"
-        echo "This is theItem: '$currentItem'"
-        choices=( ${choices[@]/$currentItem} ) # Remove currentItem from items
         newText=$(echo "$currentItem" | sed 's/ ✔️//')
     else
         newText="${items[$index]} ✔️"
-        choices+=( "$newText" )
     fi
 
     items[$index]=$newText
+}
+
+listChoices() {
+    for element in "${items[@]}";
+    do
+        if grep -q "✔️" <<< "$element"; then # If currentItem has a ✔️
+            echo $element
+        fi
+    done
 }
 
 showMenu(){
@@ -27,21 +31,13 @@ showMenu(){
         do
             for selection in $REPLY
             do
-                echo "selection: $selection"
                 [ $selection == $((${#items[@]}+1)) ] && break 3; # Quit will always be the last option
                 toggleItem $selection
             done
-
-            echo "array: ${choices[@]}"
             break;
         done
     done
 
-    echo "Choice list: ${choices[*]}"
-    for el in $choices
-    do
-        echo "Your choices:"
-        echo $el
-    done
+    listChoices
 }
 showMenu
