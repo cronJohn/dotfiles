833b970fbe2c91bd4480524fcdba76f8d97978b5
cronJohn <51012201+cronJohn@users.noreply.github.com>
2023-01-09 12:03:52 -0600

    Moved functions into separate utils.sh file
    Resolves issues where certain functions are not found. Could be fixed by putting functions at the top, but I also want the config arrays to be at the top

--- software_install.sh
+++ software_install.sh
@@ -3,6 +3,8 @@
 # Exit if any error happens
 set -e
 
+source "./utils.sh"
+
 standard_programs=( $(lts neovim) zsh python3-neovim kate steam discord inkscape qalculate-qt ripgrep fzf)
 
 flatpak_programs=( net.ankiweb.Anki com.obsproject.Studio )
@@ -13,82 +15,7 @@ node_version=latest # lts or latest
 
 zshrc_location=$(find $HOME -name ".zshrc" -not -path '*/*dotfiles/*')
 
-
-get_linux_distro() {
-    location="/etc/os-release"
-    for distro in "${linux_distros[@]}"
-    do
-        if grep -q $distro $location # finds something
-        then
-            echo "$distro"
-        fi
-    done
-}
-
-lts() {
-    Repo="${2:-updates}" # Default repo is updates
-    echo $1-$(get_latest_package_version $1 $Repo)
-}
-
-get_latest_package_version() {
-    Package=$1
-    Repo=$2
-    PackageInfo=$(dnf list --showduplicates $Package | grep -e "$Repo")
-    temp_array=($(echo $PackageInfo | tr " " "\n")) # Split by space
-    echo ${temp_array[1]}
-}
-
-get_installation_method() {
-    user_distro=$(get_linux_distro)
-    command="sudo "
-
-    if [ $user_distro = "fedora" ] || [ $user_distro = "nobara" ];
-    then
-        command+="dnf install"
-    elif [ $user_distro = "arch" ];
-    then
-        command+="pacman -S"
-    elif [ $user_distro = "ubuntu" ];
-    then
-        command+="apt install"
-    else
-        echo "User distro unknown"
-    fi
-    echo $command
-}
-
 installation_method=$(get_installation_method)
-
-install_programs(){
-    # Install all standard programs
-    for i in "${standard_programs[@]}"
-    do
-        eval "$installation_method $i"
-    done
-
-    # Install all flatpaks
-    for i in "${flatpak_programs[@]}"
-    do
-        flatpak install flathub $i
-    done
-}
-
-
-install_font() {
-    git clone https://github.com/ronniedroid/getnf.git
-    cd getnf
-    ./install.sh
-    getnf
-    cd ..
-    rm -rf getnf
-}
-
-run_dotfile_setups() {
-    for dir in */;
-    do
-        $dir/setup.sh
-    done
-}
 
 # Main script execution
 run_dotfile_setups
