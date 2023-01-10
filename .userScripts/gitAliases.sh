8c3e5ff5416106ed588456718c7b1a1adfee8cf1
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2023-01-10 17:44:42 -0600

    Added Git aliases file

---
+++ .userScripts/gitAliases.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IFS=$'\n'
+
+cmds="
+co checkout
+br branch
+ci commit
+last 'log -1 HEAD'
+r reset
+rh reset --hard
+ac '!git add -A && git commit -m'
+s status
+d diff
+"
+
+for entry in $cmds;
+do
+    eval "git config --global alias.$entry"
+done
+
+
+IFS=' '
