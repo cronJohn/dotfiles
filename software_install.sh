14d319035991d582e4dbcf7fc9f2cbf3fa66398a
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2023-01-10 12:58:57 -0600

    Prefixed zshrc_location variable usages with $
    Now uses the value in the variable

--- software_install.sh
+++ software_install.sh
@@ -26,13 +26,13 @@ zshrc_location=$(find $HOME -name ".zshrc" -not -path '*/*dotfiles/*')
 
 # A universal way of installing rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo 'source "$HOME/.cargo/env"' >> zshrc_location
+echo 'source "$HOME/.cargo/env"' >> $zshrc_location
 
 
 # A universal way of installing pnpm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
-echo 'export PNPM_HOME="$HOME/.local/share/pnpm"' >> zshrc_location
-echo 'export PATH="$PNPM_HOME:$PATH"' >> zshrc_location
+echo 'export PNPM_HOME="$HOME/.local/share/pnpm"' >> $zshrc_location
+echo 'export PATH="$PNPM_HOME:$PATH"' >> $zshrc_location
 
 pnpm env use --global $node_version
 
