62eef2e2e47f1abe3439ae478c0e81370ff93462
cronJohn <51012201+cronJohn@users.noreply.github.com>
2023-01-19 21:38:42 -0600

    Added separate plugins directory for lazy.nvim

--- nvim/lua/main/lazy.lua
+++ nvim/lua/main/lazy.lua
@@ -15,42 +15,4 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 
-return require('lazy').setup({
-
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {'nvim-lua/plenary.nvim'},
-  },
-
-  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
-  'nvim-treesitter/playground',
-  'ThePrimeagen/harpoon',
-  'mbbill/undotree',
-  'tpope/vim-fugitive',
-
-  -- LSP
-  {
-	  'VonHeikemen/lsp-zero.nvim',
-	  dependencies = {
-		  -- LSP Support
-		  'neovim/nvim-lspconfig',
-		  'williamboman/mason.nvim',
-		  'williamboman/mason-lspconfig.nvim',
-
-		  -- Autocompletion
-		  'hrsh7th/nvim-cmp',
-		  'hrsh7th/cmp-buffer',
-		  'hrsh7th/cmp-path',
-		  'saadparwaiz1/cmp_luasnip',
-		  'hrsh7th/cmp-nvim-lsp',
-		  'hrsh7th/cmp-nvim-lua',
-
-		  -- Snippets
-		  'L3MON4D3/LuaSnip',
-		  'rafamadriz/friendly-snippets',
-  	},
-  },
-
-  -- Theme
-  'navarasu/onedark.nvim',
-})
+require('lazy').setup('plugins')
