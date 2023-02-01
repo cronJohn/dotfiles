5b7314461ea42c928d3d794dc8f38503223a81af
cronJohn <51012201+cronJohn@users.noreply.github.com>
2023-02-01 13:39:48 -0600

    Updated pnpm setup file to properly use ~
    Bash tilde expansion doesn't work if it is in a "string". This fixes an issue where pnpm isn't found because the environment variable doesn't equal the expanded home directory path, thus, giving it an unknown path.

--- .userScripts/installPnpm.sh
+++ .userScripts/installPnpm.sh
@@ -5,7 +5,7 @@ node_version=latest # lts or latest
 
 # A universal way of installing pnpm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
-echo 'export PNPM_HOME="~/.local/share/pnpm"' >> $zshrc_location
+echo 'export PNPM_HOME=~/.local/share/pnpm' >> $zshrc_location
 echo 'export PATH="$PNPM_HOME:$PATH"' >> $zshrc_location
 
 source ~/.bashrc
