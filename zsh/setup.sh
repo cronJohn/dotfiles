8151af14ddde2301644398863546a13fb1f59402
cronJohn <51012201+cronJohn@users.noreply.github.com>
2023-05-31 23:40:28 -0500

    Use zprofile instead of zshenv
    Should be more appropriate to target login shells instead of all zsh instances because this is a user-specific configuration

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,10 +2,10 @@
 
 # Make sure this file is executable
 
-# Source .zshenv to set XDG_CONFIG_HOME environment variables
-source zsh/.zshenv
+# Source .zprofile to set XDG_CONFIG_HOME environment variables
+source zsh/.zprofile
 
-# No .zshrc file will be at $HOME, so .zshenv file will be in the home directory
-mv zsh/.zshenv $HOME
+# No .zshrc file will be at $HOME, so .zprofile file will be in the home directory
+mv zsh/.zprofile $HOME
 
 mv zsh $XDG_CONFIG_HOME
