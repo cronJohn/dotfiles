f202b6b67ac6a132c1c4fc4497e48da4de0b08d6
cronJohn <51012201+cronJohn@users.noreply.github.com>
2023-09-28 13:52:07 -0500

    Add nvim module for autoinstalling things
    Mainly used to autoinstall debuggers like codelldb

---
+++ nvim/lua/core/plugin-config/auto-install.lua
@@ -0,0 +1,17 @@
+local packages_to_install = {
+    "codelldb",
+}
+
+local function install_package(package)
+    vim.cmd("MasonInstall " .. package)
+end
+
+local function is_installed(package)
+    return vim.fn.executable(package) == 1
+end
+
+for _, package in ipairs(packages_to_install) do
+    if not is_installed(package) then
+        install_package(package)
+    end
+end
