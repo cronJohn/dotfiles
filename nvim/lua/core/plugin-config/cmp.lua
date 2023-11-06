3aa570361455c3b5314a150cac11859126cbb4ad
cronJohn <51012201+cronJohn@users.noreply.github.com>
2023-11-06 12:17:14 -0600

    Add search and cmdline completion for nvim

--- nvim/lua/core/plugin-config/cmp.lua
+++ nvim/lua/core/plugin-config/cmp.lua
@@ -23,6 +23,23 @@ cmp.setup({
   }),
 })
 
+ -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+ cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
 -- Icons
 local lspkind = require('lspkind')
 cmp.setup {
